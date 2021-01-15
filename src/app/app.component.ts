3516fe8b24a2d33c68798c7e4f76195a3eedf3d5
Nicholas Martinez <nicok.martinez@gmail.com>
2021-01-15 15:26:54 -0500

    Made it so that the best image is shown based on the screen size.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NavigationEnd, Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'improved-portfolio-site';
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    // Primarily for mobile devices. If clicking one of the links in the descriptions,
+    // It will scroll back to the top of the page so the user is not in the middle of the new page.
+    this.router.events.subscribe((routeChangedEvent) => {
+      if (!(routeChangedEvent instanceof NavigationEnd)) {
+        return;
+      }
+      window.scrollTo(0, 0);
+    });
+  }
 }
